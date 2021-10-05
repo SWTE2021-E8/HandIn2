b7e2c9c06ab66875f060cfac4dfd6732d5247db9
phil2988 <phillipblicher@hotmail.com>
2021-10-05 08:15:34 +0100

    Logger + Door Started

---
+++ Ladeskab - Mobile charging station/Ladeskab - Mobile charging station/Door.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab___Mobile_charging_station
+{
+    public class DoorStateChangedEventArgs : EventArgs
+    {
+        public bool State { get; set; }
+    }
+
+    public class Door : IDoor
+    {
+        bool currentState;
+        bool isLocked;
+        public event EventHandler<DoorStateChangedEventArgs> DoorStateChangedEvent;
+
+        public void SetDoorState(bool state)
+        {
+            if(state != currentState)
+            {
+                OnDoorOpen(new DoorStateChangedEventArgs { State = state });
+                currentState = state;
+            }
+        }
+        
+        protected virtual void OnDoorOpen(DoorStateChangedEventArgs e)
+        {
+            DoorStateChangedEvent?.Invoke(this, e);
+        }
+    }
+
+    public interface IDoor
+    {
+        event EventHandler<DoorStateChangedEventArgs> DoorStateChangedEvent;
+    }
+}
---
+++ Ladeskab - Mobile charging station/Ladeskab - Mobile charging station/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab___Mobile_charging_station
+{
+    class Logger
+    {
+        public class LogItem
+        {
+            public string text { get; private set; }
+            public DateTime time { get; private set; }
+            public LogItem(string s, DateTime d)
+            {
+                time = d;
+                text = s;
+            }
+        }
+        public List<LogItem> log { get; private set; }
+
+        public void AddToLog(string logEntry)
+        {
+            log.Add(new LogItem(logEntry, DateTime.Now));
+        }
+    }
+}

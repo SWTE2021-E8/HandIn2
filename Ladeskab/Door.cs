265e683e162cb3aef417647cd590282dbb73e684
Oliver Hennings <89971830+OliverSchriverHennings@users.noreply.github.com>
2021-10-07 10:38:25 +0200

    Update Door.cs

--- Ladeskab/Door.cs
+++ Ladeskab/Door.cs
@@ -4,20 +4,53 @@ using Ladeskab.Interfaces;
 namespace Ladeskab {
     public class Door : IDoor {
 
+		private IStationControl stationControl;
+
+        public DoorState StateValue { get; private set; }
+
+        public event EventHandler<DoorEventArgs> DoorValueEvent;
+
+        public Door(IStationControl stationControl) {
+			this.stationControl = stationControl;
+		}
+			
+
 	public void OnDoorOpen() {
-			System.Console.WriteLine("Door Open");
+			//stationControl.Open
+			if (StateValue == DoorState.Unlocked){
+				StateValue = DoorState.Dooropening;
+				OnNewDoorEvent();
+				System.Console.WriteLine("Door Open");
+			}
 	}
 
 	public void OnDoorClose() {
-			System.Console.WriteLine("Door Close");
+			if (StateValue == DoorState.Dooropening)
+			{
+				StateValue = DoorState.DoorClosing;
+				OnNewDoorEvent();
+				System.Console.WriteLine("Door closed");
+			}
 		}
 		
 	public void LockDoor() {
-			System.Console.WriteLine("Door Locked");
+			if (StateValue == DoorState.DoorClosing)
+            {
+				StateValue = DoorState.Locked;
+				System.Console.WriteLine("Door Locked");
+			}
 	}
 
 	public void UnlockDoor() {
-			System.Console.WriteLine("Door Unlocked");
-	}
+			if (StateValue == DoorState.Locked)
+			{
+				StateValue = DoorState.Unlocked;
+				System.Console.WriteLine("Door Unlocked");
+			}
+		}
+		private void OnNewDoorEvent()
+        {
+			DoorValueEvent?.Invoke(this, new DoorEventArgs() { Doorstate = this.StateValue });
+        }
     }
 }

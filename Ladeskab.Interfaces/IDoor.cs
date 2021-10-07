21dd0dc9425d30ea79603179aca7a4c74adf2529
Oliver Hennings <89971830+OliverSchriverHennings@users.noreply.github.com>
2021-10-07 10:56:15 +0200

    enum + door changes

--- Ladeskab.Interfaces/IDoor.cs
+++ Ladeskab.Interfaces/IDoor.cs
@@ -1,10 +1,27 @@
 using System;
 
 namespace Ladeskab.Interfaces {
+
+    public enum DoorState
+    {
+        Dooropening,
+        DoorClosing,
+        Locked,
+        Unlocked
+    }
+    public class DoorEventArgs : EventArgs
+    {
+        public DoorState Doorstate { get; set; }
+    }
     public interface IDoor {
+
+        event EventHandler<DoorEventArgs> DoorValueEvent;
+        DoorState StateValue { get; }
+
         public void LockDoor();
         public void UnlockDoor();
         public void OnDoorOpen();
         public void OnDoorClose();
+
     }
 }

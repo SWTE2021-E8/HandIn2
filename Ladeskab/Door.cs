4473e6e7bfa067c7bcaeb3e683db0d327d01ffcd
Oliver Hennings <89971830+OliverSchriverHennings@users.noreply.github.com>
2021-10-07 09:26:15 +0200

    added door attribut

--- Ladeskab/Door.cs
+++ Ladeskab/Door.cs
@@ -3,21 +3,23 @@ using Ladeskab.Interfaces;
 
 namespace Ladeskab {
     public class Door : IDoor {
+		string DoorStatus = "Open";
+		string LockStatus = "unlocked";
 
 	public void OnDoorOpen() {
-
+			DoorStatus = "Door Open";
 	}
 
 	public void OnDoorClose() {
-
-	}
+			DoorStatus = "Door Close";
+		}
 
 	public void LockDoor() {
-
+			LockStatus = "Door Locked";
 	}
 
 	public void UnlockDoor() {
-
+			LockStatus = "Door Unlocked";
 	}
     }
 }

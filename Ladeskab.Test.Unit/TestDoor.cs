60e11489511c50f8fead486d55adf97d4606a72e
Oliver Hennings <89971830+OliverSchriverHennings@users.noreply.github.com>
2021-10-12 09:13:11 +0200

    Create a TestDoor

---
+++ Ladeskab.Test.Unit/TestDoor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class Testdoor
+    {
+        public Door uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Door();
+        }
+
+
+        [Test]
+        public void DoorIsUnlockedAtStart()
+        {
+            Assert.AreEqual(uut.StateValue,DoorState.Unlocked);
+        }
+
+        [Test]
+        public void DoorGetsLockedAndUnlocked()
+        {
+            uut.LockDoor();
+            Assert.AreEqual(uut.StateValue, DoorState.Locked);
+
+            uut.UnlockDoor();
+            Assert.AreEqual(uut.StateValue,DoorState.Unlocked);
+
+        }
+
+        [Test]
+        public void DoorClosed()
+        {
+            uut.OnDoorClose();
+            Assert.AreEqual(uut.StateValue, DoorState.Unlocked);
+        }
+
+        [Test]
+        public void DoorOpen()
+        {
+            uut.OnDoorOpen();
+            Assert.AreEqual(uut.StateValue,DoorState.DoorOpen);
+        }
+    }
+
+}

702655ccf488e9f4fb4ea9ac7b1625e68e0041e2
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-10-14 11:10:41 +0200

    Added tests for ChargeControl

--- Ladeskab.Test.Unit/ChargeControlTests.cs
+++ Ladeskab.Test.Unit/ChargeControlTests.cs
@@ -18,6 +18,7 @@ namespace Ladeskab.Test.Unit
             ChargeControl _uut;
             IDisplay display;
             IUsbCharger charger;
+
             [SetUp]
             public void Setup()
             {
@@ -32,12 +33,14 @@ namespace Ladeskab.Test.Unit
                 charger.Connected.Returns(true);
                 Assert.IsTrue(_uut.Connected);
             }
+
             [Test]
             public void ChargeControl_IsConnected_returnFalse()
             {
                 charger.Connected.Returns(false);
                 Assert.IsFalse(_uut.Connected);
             }
+
             [Test]
             public void ChargeControl_Current_ReturnValue()
             {
@@ -46,8 +49,9 @@ namespace Ladeskab.Test.Unit
 
                 charger.CurrentValueEvent += Raise.EventWith(currentEventArgs);
 
-                Assert.AreEqual(100, _uut.Current);                                                                      
+                Assert.AreEqual(100, _uut.Current);
             }
+
             [Test]
             public void ChargeControl_ChargeControlState_ReturnCharging()
             {
@@ -58,9 +62,10 @@ namespace Ladeskab.Test.Unit
 
                 Assert.AreEqual(ChargeControl.ChargeControlState.Charging, _uut.chargeControlState);
             }
+
             [Test]
             public void ChargeControl_ChargeControlState_ReturnIdle()
-            {   
+            {
                 CurrentEventArgs currentEventArgs = new CurrentEventArgs();
                 currentEventArgs.Current = 500;
 
@@ -68,6 +73,7 @@ namespace Ladeskab.Test.Unit
 
                 Assert.AreEqual(ChargeControl.ChargeControlState.Idle, _uut.chargeControlState);
             }
+
             [Test]
             public void ChargeControl_ChargeControlState_ReturnError()
             {
@@ -79,18 +85,30 @@ namespace Ladeskab.Test.Unit
                 Assert.AreEqual(ChargeControl.ChargeControlState.Error, _uut.chargeControlState);
             }
 
-            //public void ChargeControl_StopCharge_WasNotChargingNowIsCharging()
-            //{
-            //    CurrentEventArgs currentEventArgs = new CurrentEventArgs();
-            //    currentEventArgs.Current = 150;
-            //    charger.CurrentValueEvent += Raise.EventWith(currentEventArgs);
+            [Test]
+            public void ChargeControl_Starts_Charging_OnDemand()
+            {
+                _uut.StartCharge();
+                charger.Received().StartCharge();
+            }
 
-            //    _uut.StopCharge();
-            //    Assert.IsTrue(charger.CurrentValue == 0);
+            [Test]
+            public void ChargeControl_Stops_Charging_OnDemand()
+            {
+                _uut.StopCharge();
+                charger.Received().StopCharge();
+            }
 
-            //    _uut.StartCharge();
-            //    Assert.IsTrue(charger.CurrentValue != 0);
-            //}
+            [Test]
+            public void ChargeControl_Stops_Charging_When_Done()
+            {
+                CurrentEventArgs currentEventArgs = new CurrentEventArgs();
+                currentEventArgs.Current = 2;
+
+                charger.CurrentValueEvent += Raise.EventWith(currentEventArgs);
+
+                Assert.AreEqual(ChargeControl.ChargeControlState.Idle, _uut.chargeControlState);
+            }
         }
     }
 }
